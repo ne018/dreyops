08d7cc0ac37f29eeed878d3040c5423bc0b187e2
drey018 <drey01814@gmail.com>
2020-05-05 21:09:16 +0800

    Update replaceassets_jitsi.sh

--- replaceassets_jitsi.sh
+++ replaceassets_jitsi.sh
@@ -2,47 +2,47 @@
 # author : drey
 # update web design
 
-sudo rm /usr/share/jitsi-meet/base.html
-sudo rm /usr/share/jitsi-meet/body.html
-sudo rm /usr/share/jitsi-meet/favicon.ico
-sudo rm /usr/share/jitsi-meet/head.html
-sudo rm /usr/share/jitsi-meet/index.html
-sudo rm /usr/share/jitsi-meet/interface_config.js
-sudo rm /usr/share/jitsi-meet/logging_config.js
-sudo rm /usr/share/jitsi-meet/package-lock.json
-sudo rm /usr/share/jitsi-meet/package.json
-sudo rm /usr/share/jitsi-meet/plugin.head.html
-sudo rm /usr/share/jitsi-meet/robots.txt
-sudo rm /usr/share/jitsi-meet/title.html
+sudo rm -v /usr/share/jitsi-meet/base.html
+sudo rm -v /usr/share/jitsi-meet/body.html
+sudo rm -v /usr/share/jitsi-meet/favicon.ico
+sudo rm -v /usr/share/jitsi-meet/head.html
+sudo rm -v /usr/share/jitsi-meet/index.html
+sudo rm -v /usr/share/jitsi-meet/interface_config.js
+sudo rm -v /usr/share/jitsi-meet/logging_config.js
+sudo rm -v /usr/share/jitsi-meet/package-lock.json
+sudo rm -v /usr/share/jitsi-meet/package.json
+sudo rm -v /usr/share/jitsi-meet/plugin.head.html
+# sudo rm /usr/share/jitsi-meet/robots.txt
+sudo rm -v /usr/share/jitsi-meet/title.html
 
-sudo rm -r /usr/share/jitsi-meet/lang
-sudo rm -r /usr/share/jitsi-meet/css
-sudo rm -r /usr/share/jitsi-meet/fonts
-sudo rm -r /usr/share/jitsi-meet/images
-sudo rm -r /usr/share/jitsi-meet/libs
-sudo rm -r /usr/share/jitsi-meet/static
-sudo rm -r /usr/share/jitsi-meet/sounds
-sudo rm -r /usr/share/jitsi-meet/connection_optimization
+sudo rm -rv /usr/share/jitsi-meet/lang
+sudo rm -rv /usr/share/jitsi-meet/css
+sudo rm -rv /usr/share/jitsi-meet/fonts
+sudo rm -rv /usr/share/jitsi-meet/images
+sudo rm -rv /usr/share/jitsi-meet/libs
+sudo rm -rv /usr/share/jitsi-meet/static
+sudo rm -rv /usr/share/jitsi-meet/sounds
+sudo rm -rv /usr/share/jitsi-meet/connection_optimization
 
 
-sudo cp base.html  /usr/share/jitsi-meet/base.html
-sudo cp body.html  /usr/share/jitsi-meet/body.html
-sudo cp favicon.ico  /usr/share/jitsi-meet/favicon.ico
-sudo cp head.html  /usr/share/jitsi-meet/head.html
-sudo cp index.html  /usr/share/jitsi-meet/index.html
-sudo cp interface_config.js  /usr/share/jitsi-meet/interface_config.js
-sudo cp logging_config.js  /usr/share/jitsi-meet/logging_config.js
-sudo cp package-lock.json  /usr/share/jitsi-meet/package-lock.json
-sudo cp package.json  /usr/share/jitsi-meet/package.json
-sudo cp plugin.head.html  /usr/share/jitsi-meet/plugin.head.html
-sudo cp robots.txt  /usr/share/jitsi-meet/robots.txt
-sudo cp title.html  /usr/share/jitsi-meet/title.html
+sudo cp -v base.html  /usr/share/jitsi-meet/base.html
+sudo cp -v body.html  /usr/share/jitsi-meet/body.html
+sudo cp -v favicon.ico  /usr/share/jitsi-meet/favicon.ico
+sudo cp -v head.html  /usr/share/jitsi-meet/head.html
+sudo cp -v index.html  /usr/share/jitsi-meet/index.html
+sudo cp -v interface_config.js  /usr/share/jitsi-meet/interface_config.js
+sudo cp -v logging_config.js  /usr/share/jitsi-meet/logging_config.js
+sudo cp -v package-lock.json  /usr/share/jitsi-meet/package-lock.json
+sudo cp -v package.json  /usr/share/jitsi-meet/package.json
+sudo cp -v plugin.head.html  /usr/share/jitsi-meet/plugin.head.html
+# sudo cp robots.txt  /usr/share/jitsi-meet/robots.txt
+sudo cp -v title.html  /usr/share/jitsi-meet/title.html
 
-sudo cp -r lang /usr/share/jitsi-meet/lang
-sudo cp -r css /usr/share/jitsi-meet/css
-sudo cp -r fonts /usr/share/jitsi-meet/fonts
-sudo cp -r images /usr/share/jitsi-meet/images
-sudo cp -r libs /usr/share/jitsi-meet/libs
-sudo cp -r static /usr/share/jitsi-meet/static
-sudo cp -r sounds /usr/share/jitsi-meet/sounds
-sudo cp -r connection_optimization /usr/share/jitsi-meet/connection_optimization
+sudo cp -rv lang /usr/share/jitsi-meet/lang
+sudo cp -rv css /usr/share/jitsi-meet/css
+sudo cp -rv fonts /usr/share/jitsi-meet/fonts
+sudo cp -rv images /usr/share/jitsi-meet/images
+sudo cp -rv libs /usr/share/jitsi-meet/libs
+sudo cp -rv static /usr/share/jitsi-meet/static
+sudo cp -rv sounds /usr/share/jitsi-meet/sounds
+sudo cp -rv connection_optimization /usr/share/jitsi-meet/connection_optimization
