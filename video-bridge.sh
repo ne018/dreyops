c0edbc0f4f339e51a22ee724a22dd44bc0196efd
drey018 <drey01814@gmail.com>
2020-06-24 23:43:42 +0800

    Update and rename jvb to video-bridge.sh

--- video-bridge.sh
+++ video-bridge.sh
@@ -1,8 +1,13 @@
+# make sure you are root!
+
 apt update
 apt upgrade -y
+
 echo 'deb https://download.jitsi.org stable/' >> /etc/apt/sources.list.d/jitsi-stable.list
 apt-get install apt-transport-https -y
 apt-get install gnupg -y
+
 wget -qO -  https://download.jitsi.org/jitsi-key.gpg.key | sudo apt-key add -
 apt update
+
 apt-get -y install jitsi-videobridge2
