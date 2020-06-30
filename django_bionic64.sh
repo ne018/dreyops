3c8fc9a28fadc5c7e23ba3bb685a2eb3b1e267f4
drey018 <drey01814@gmail.com>
2020-06-30 11:41:50 +0800

    Create django_bionic64.sh

---
+++ django_bionic64.sh
@@ -0,0 +1,30 @@
+# python3 ubuntu bionic64
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt update
+
+sudo apt install git unzip curl -y
+
+sudo apt install software-properties-common
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+
+
+sudo apt install python3.8 -y
+sudo apt install python3-pip -y
+
+# python3 -V
+
+# install django
+# pip3 install django
+
+# verify version
+# python3 -m django --version
+
+# create a django project
+# python3 -m django startproject mydjangoapp
+
+
+# testing for development server via vagrant
+# make sure you've open the port you will use
